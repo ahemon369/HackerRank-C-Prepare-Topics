7bcdb344db2c6e93eaccb23eef5b9a370f8d7ffd
MD AMRAN HOSSIN EMON <mdamranhossinemon78@gmail.com>
2024-11-29 01:55:36 +0600

    Add files via upload

---
+++ HackerRank C++ Prepare Topics/Say_Hello_World_With_C.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    char s[100];
+    cin >> s;
+ 
+    cout <<"Hello, " << s <<endl;
+    
+}   
+
+
+                /*-----------------------------------------------
+                            THIS CODE WAS WRITTEN 
+                                   BY
+                            MD AMRAN HOSSIN EMON
+                
+                -------------------------------------------------*/
